db6c96d816331f4eca878ea4822123d3a4ed135f
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-01-12 09:10:47 +0100

    attempted to fix IG refresh cronjob

--- supabase/migrations/202505311547000_Cron.sql
+++ supabase/migrations/202505311547000_Cron.sql
@@ -16,26 +16,26 @@ SELECT
     $$
     DECLARE
       v_project_url TEXT;
-      v_service_key TEXT;
+--       v_service_key TEXT;
       v_function_target_url TEXT;
     BEGIN
       SELECT decrypted_secret INTO v_project_url FROM vault.decrypted_secrets WHERE name = 'CRON_SUPABASE_PROJECT_URL';
-      SELECT decrypted_secret INTO v_service_key FROM vault.decrypted_secrets WHERE name = 'CRON_SUPABASE_SERVICE_KEY';
-
-      IF v_project_url IS NULL THEN
-        RAISE EXCEPTION 'Secret CRON_SUPABASE_PROJECT_URL not found in Vault.';
-      END IF;
-      IF v_service_key IS NULL THEN
-        RAISE EXCEPTION 'Secret CRON_SUPABASE_SERVICE_KEY not found in Vault.';
-      END IF;
+--       SELECT decrypted_secret INTO v_service_key FROM vault.decrypted_secrets WHERE name = 'CRON_SUPABASE_SERVICE_KEY';
+--
+--       IF v_project_url IS NULL THEN
+--         RAISE EXCEPTION 'Secret CRON_SUPABASE_PROJECT_URL not found in Vault.';
+--       END IF;
+--       IF v_service_key IS NULL THEN
+--         RAISE EXCEPTION 'Secret CRON_SUPABASE_SERVICE_KEY not found in Vault.';
+--       END IF; TODO
 
       v_function_target_url := rtrim(v_project_url, '/') || '/functions/v1/fetchInstagramPostsAndStore';
 
       PERFORM net.http_post(
           url := v_function_target_url,
           headers := jsonb_build_object(
-            'apikey', v_service_key,
-            'Authorization', 'Bearer ' || v_service_key,
+--             'apikey', v_service_key,
+--             'Authorization', 'Bearer ' || v_service_key,
             'Content-Type', 'application/json'
           ),
           body := '{}'::jsonb,
